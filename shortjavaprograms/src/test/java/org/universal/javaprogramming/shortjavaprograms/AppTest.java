55b981b64451d1272519b8f79187126bc0c2833c
nikhilchugh2001 <nikhil.chugh2001@gmail.com>
2021-03-27 21:09:33 +0530

    initial commit

---
+++ shortjavaprograms/src/test/java/org/universal/javaprogramming/shortjavaprograms/AppTest.java
@@ -0,0 +1,20 @@
+package org.universal.javaprogramming.shortjavaprograms;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
